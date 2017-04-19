b655f9eb70fecd186761a2b55bd4de85fedf9829
Arnaud <simplon@simplon-Latitude-E6410>
2017-04-19 17:09:03 +0200

    Nouvelle fonction direBonjour

--- Prairie2/src/Main.java
+++ Prairie2/src/Main.java
@@ -1,12 +1,16 @@
 
 public class Main {
+	
+	public static void direBonjour (){
+		System.out.println("Hello");
+		System.out.println("Hello Dori !");
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hello Dori !");
+
 		
 	}
 
